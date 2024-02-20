5d50416f66edf56d3a630c3733a7de14c17f4c6e
AbhishekRaj111001 <avirock912@gmail.com>
2024-02-20 18:30:31 +0530

    321-GO

---
+++ Assets/Level 1 scripts/PlayerScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public Rigidbody rigidbody;
+    public float F = 20f;
+    public float M = 8f;
+    
+    public float maxX;
+    public float minX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerPos = transform.position;
+
+        if(playerPos.x<minX)
+        {
+            playerPos.x = minX;
+        }
+        transform.position = playerPos;
+
+        if(playerPos.x>maxX)
+        {
+            playerPos.x = maxX;
+        }
+        transform.position = playerPos;
+
+        if(Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position = transform.position + new Vector3(0, 0, M * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position = transform.position + new Vector3(-M * Time.deltaTime, 0, 0);
+        }
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position = transform.position + new Vector3(M * Time.deltaTime, 0, 0);
+        }
+        if(Input.GetKey(KeyCode.Space))
+        {
+            transform.position = transform.position + new Vector3(0,M * Time.deltaTime, 0);
+        }
+    }
+    private void FixedUpdate()
+    {
+        transform.position = transform.position + new Vector3(0, 0, F * Time.deltaTime);
+    }
+}
---
+++ Assets/Level 1 scripts/camerascrift.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camerascrift : MonoBehaviour
+{
+    public Transform followplayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 camerapos = transform.position;
+        camerapos.z = followplayer.position.z;
+    }
+}
